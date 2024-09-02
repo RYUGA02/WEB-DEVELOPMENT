410d423a1431826284662852418727c252a7f8cc
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-02 17:43:29 +0530

    Add files via upload

---
+++ Javascript Switch Case Statements/index.js
@@ -0,0 +1,22 @@
+// Switch Statement
+
+let weight = 500;
+switch (weight) {
+    case 50: console.log("You are just need small exercise routine to be Fit.");
+    break;
+    case 60: console.log("You are Fit as Gym Wrestler!");
+    break;
+    case 40: console.log("You are underweight boy!");
+    break;
+    case 30: console.log("You need Miracle !");
+    break;
+    case 20: console.log("Already Dead!");
+    break;
+    case 70: console.log("Fat as Dawg!");
+    break;
+    case 80: console.log("Earth's Weight!");
+    break;
+    case 100: console.log("Earth + Moon Weight!");
+    break;
+    case 500: console.log("Universe Poop!");
+}
