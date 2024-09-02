de12532ab7b9f03cf4190a2e18c3bb759cbd3761
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-02 17:43:00 +0530

    Add files via upload

---
+++ Javascript If-else Conditional Statements/index.js
@@ -0,0 +1,32 @@
+// if Condition statement
+let speed = 60;
+if (speed >= 60) {
+    console.log("Sir! you will get Charged for overspeeding.");
+}
+
+
+// If-else Statements
+if (speed > 60) {
+    console.log("You are fined!");
+}
+else {
+    console.log("you are not fined!");
+}
+
+
+// if-else-if Ladder statement in Javascript
+
+let age = 1;
+if (age >= 18) {
+    console.log("You can Vote!");
+}
+else if (age < 18 && age >= 10) {
+    console.log("You can Vote for Teenagers Party");
+}
+else if (age >= 5 && age < 10) {
+    console.log("You can Vote for Children Party");
+}
+else {
+    console.log("You are Baby. Just Eat and Sleep!");
+}
+
