993ae7e9be2c595b6d660063371b3b6b5d6a50cc
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-15 19:52:12 +0530

    Add files via upload

---
+++ Javascript In-Built Objects/index.js
@@ -0,0 +1,57 @@
+// There are 2 In-Built Objects in Javascript
+
+// 1. Math Object
+
+// console.log(Math.random());
+
+// console.log(Math.round(1.4));
+
+// console.log(Math.round(1.6));
+
+// console.log(Math.max(123,335354,546,5657,76776,87,54,546676,45675,55,565,76,4546,66756));
+
+// console.log(Math.min(1223,24,55,66,7,8,99,7,87,66));
+
+// console.log(Math.sqrt(64));
+
+// console.log(Math.pow(8,5));
+
+// console.log(Math.PI);
+
+// console.log(Math.abs(-4));
+
+// console.log(Math.floor(1.9));
+
+// console.log(Math.ceil(1.1));
+
+// console.log(Math.sin(60));
+
+// console.log(Math.cos(60));
+
+// console.log(Math.log(10));
+
+
+// 2. Date Object
+
+let newDate = new Date(2002, 2, 10, 8, 15, 24);
+console.log(newDate);
+
+
+console.log(newDate.getFullYear());
+
+console.log(newDate.getDate());
+
+console.log(newDate.getDay());
+
+console.log(newDate.getHours());
+
+console.log(newDate.getMilliseconds());
+
+console.log(newDate.getMinutes());
+
+console.log(newDate.getMonth());
+
+console.log(newDate.getUTCDate());
+
+console.log(newDate.getUTCDay());
+
