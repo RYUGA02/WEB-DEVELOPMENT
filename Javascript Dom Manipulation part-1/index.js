43a4b5450cd0e92dc384a8ebd4350a370f8a07da
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-19 00:30:01 +0530

    Add files via upload

---
+++ Javascript Dom Manipulation part-1/index.js
@@ -0,0 +1,43 @@
+// console.log("hello");
+
+// // Console Methods
+
+// // 1.console.error();
+//  // Store number to variable
+// let num = 200
+
+// // Check condition
+// if (num < 100) {
+//     console.log("Enter number greater than 100");
+// }
+// else {
+//     console.error("correct choice");
+// }
+
+// // 2.console.warn();
+// console.warn("Hello ! Eren please kill everyone!");
+
+// // 3.console.clear()
+// // console.clear();
+
+// // 4.console.info();
+// console.info("What are you doing my man?");
+
+
+
+// Javascript Dom Manipulation
+
+// ChildNodes 
+
+console.log(document.body.childNodes[3].childNodes);
+console.log(document.body.childNodes[3].childNodes)
+console.log(document.body.childNodes[3].childNodes[1])
+console.log(document.body.childNodes[1].firstChild)
+console.log(document.body.childNodes[3].firstElementChild)
+
+// Element Only Navigation
+
+console.log(document.body.childNodes[3].previousElementSibling)
+console.log(document.body.childNodes[3].nextElementSibling)
+console.log(document.body.childNodes[3].firstElementChild)
+console.log(document.body.childNodes[3].lastElementChild)
