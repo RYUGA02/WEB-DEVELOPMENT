2df878d9fca7c931fc3c6a570201d5833bc9b2b9
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-20 18:31:49 +0530

    Add files via upload

---
+++ Javascript Other Searching Methods/index.js
@@ -0,0 +1,21 @@
+//Javascript matches(), closeset() and contains() method
+
+// method-1 : matches()
+
+// let id1 = document.getElementById("Box-container");
+// console.log(id1);
+// console.log(id1.matches(".container")); 
+
+
+// method-2 : closeset()
+
+// let id2 = document.getElementById("sp1");
+// console.log(id2);
+// console.log(id2.closest(".box1"));
+
+
+// method-3 : contains()   ---- it targets only the id tag remember always.
+
+let id3 = document.getElementById("Box-container");
+console.log(id3);
+console.log(id3.contains(boxid4));
