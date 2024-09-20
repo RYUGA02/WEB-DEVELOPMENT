969f94f511338bf1319f00fe0689516b2a3c98d7
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-20 19:29:01 +0530

    Add files via upload

---
+++ Javascript Attribute Methods/index.js
@@ -0,0 +1,35 @@
+// Javascript Attribute Methods
+
+// method-1 : hasAttribute()
+
+// let id1 = document.getElementById("box-container")
+// console.log(id1)
+// console.log(id1.hasAttribute("style"))
+
+// method-2 : getAttribute()
+
+// let id1 = document.getElementById("box-container")
+// console.log(id1.getAttribute("style"))
+
+// method-3 : setAttribute()
+
+// let id1 = document.getElementById("box-container")
+// console.log(id1)
+// console.log(id1.setAttribute("class", "heading"))
+
+
+// method-4 : removeAttribute()
+
+// let id1 = document.getElementById("box-container")
+// console.log(id1.removeAttribute("style"))
+
+// method-5 : attributes()
+
+// let id1 = document.getElementById("box-container")
+// console.log(id1.attributes)
+
+// method-6 : data-* attribute or custom attribute
+
+let id1 = document.getElementById("box-container")
+console.log(id1.dataset.kush)
+console.log(id1.dataset.virat)
