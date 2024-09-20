62b1b01b0a047ffcef705c3b83a7e454e55974f0
Kush Kumar Singh <99049838+RYUGA02@users.noreply.github.com>
2024-09-20 20:22:33 +0530

    Add files via upload

---
+++ Javascript other Dom Properties/index.js
@@ -0,0 +1,38 @@
+// Javascript Insert other dom properties
+
+// method-1 : append()
+
+// let newElement = document.createElement("h4")
+// newElement.textContent = "This Is the Information Data."
+// let id1 = document.getElementById("Box-Container")
+// id1.append(newElement)
+
+
+// method-2 : prepend()
+
+// let newElement = document.createElement("h4")
+// newElement.textContent = "This Is the Information Data."
+// let id1 = document.getElementById("Box-Container")
+// id1.prepend(newElement)
+
+// method-3 : before()
+
+// let newElement = document.createElement("h4")
+// newElement.textContent = "This Is the Information Data."
+// let id1 = document.getElementById("Box-Container")
+// id1.before(newElement)
+
+// method-4 : after()
+
+// let newElement = document.createElement("h4")
+// newElement.textContent = "This Is the Information Data."
+// let id1 = document.getElementById("Box-Container")
+// id1.after(newElement)
+
+// method-5 : replaceWith()
+
+// let newElement = document.createElement("h4")
+// newElement.textContent = "This Is the Information Data."
+// let id1 = document.getElementById("Box-Container")
+// id1.replaceWith(newElement)
+
